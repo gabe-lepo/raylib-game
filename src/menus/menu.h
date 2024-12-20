0249a8298a0766c800ebb74bb1eab6556a119e97
gabe-gardyn <gabriel@gardyn.tech>
2024-12-20 13:31:51 -0500

    basic structs

--- src/menus/menu.h
+++ src/menus/menu.h
@@ -6,6 +6,30 @@
 #include "log.h"
 #include "game.h"
 
+typedef struct Menu Menu;
+typedef struct MenuOptions MenuOptions;
+typedef struct MenuOptionsFunctions MenuOptionsFunctions;
+
+typedef struct
+{
+   const char *title;
+   MenuOptions *options[];
+
+   // Menu control/display functionality:
+   // void InitMenus(void)
+} Menu;
+
+typedef struct
+{
+   const char *name;
+   MenuOptionsFunctions *function;
+} MenuOptions;
+
+typedef struct
+{
+
+} MenuOptionsFunctions;
+
 #define START_MENU 0
 #define PAUSE_MENU 1
 #define SETTINGS_MENU 2
